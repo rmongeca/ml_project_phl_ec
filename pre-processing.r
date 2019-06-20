78c3a407119a65c2b49e888dea44e71450bed23e
Ricard Monge <rikymonge@gmail.com>
2019-06-20 13:31:38 +0200

    Updated pre-processing with missing values var ind removal.

--- pre-processing.r
+++ pre-processing.r
@@ -14,9 +14,10 @@ data <- data[which(data$P_STATUS == 3),]
 ## Remove unnecesary columns
 colnames(data)
 rownm <- data[,1]
-drop.ind <- c(1:2,4:5,7:10,12:13,15:16,18:19,21:22,24:25,27:32,35:48,50:51,53:54,56:61,63:68,73,79:83,87:89,91:98,107:109)
-drop.col <- colnames(data)[drop.ind]
-data <- data[,-drop.ind]
+d.index <- c(1:2,4:5,7:10,12:13,15:16,18:19,21:22,24:25,27:32,34:48,50:51,53:54,56:61,63:68,73,77,79:80,82:83,87:89,93:101,107:109)
+drop.col <- as.data.frame(colnames(data)[d.index])
+data <- data[,-d.index]
+rm(d.index)
 
 ## Assign rownames
 rownames(data) <- rownm
@@ -24,28 +25,35 @@ rm(rownm)
 
 ################### COLS/ROWS WITH MISSINGS REMOVAL ###################
 
-## See missing values percentage per column
-(var.na <- apply(data, 2, function(x) 100*(sum(is.na(x))/nrow(data))))
-
 ## Change planet radius, mass and semi axis for estimates (without many missings)
 ## Reorder columns, first planet's properties then star's
 colnames(data)
-order.col <- c(37,36,38,3,19:24,13:14,25:28,31:35)
+order.col <- c(35,34,36,3,5:9,14:23,10:13,24:28,29:33)
 drop.col <- drop.col %>%
   append(colnames(data)[-order.col])
 data <- data[,order.col]
 rm(order.col)
 
+## See missing values percentage per column
+(var.na <- apply(data, 2, function(x) 100*(sum(is.na(x))/nrow(data))))
+plot(var.na)
+abline(h=10, col="red")
+## Remove columns with more than 10% missings
+col.dp <- which(var.na > 10)
+drop.col <- drop.col %>%
+  append(colnames(data)[col.dp])
+data <- data[,-col.dp]
+rm(col.dp, var.na)
+
 ## See missing values percentage per row
 ind.na <- apply(data, 1, function(x) 100*(sum(is.na(x))/ncol(data)))
 plot(ind.na)
 abline(h=10, col="red")
-
 ## Drop rows with more than 10% missing values
 ind.dp <- which(ind.na > 10)
 drop.ind <- data[ind.dp,]
 data <- data[-ind.dp,]
-rm(ind.dp)
+rm(ind.dp, ind.na)
 
 ################### FURTHER PRE-PROCESSING ###################
 
@@ -59,8 +67,4 @@ plot(data$P_HABITABLE)
 
 ## Missings for habitable planets
 hab.pl <- data[which(data$P_HABITABLE > 0),]
-hab.na <- hab.pl %>%
-  apply(., 1, function(x) 100*(sum(is.na(x))/ncol(.)))
-plot(hab.na)
-abline(h=10, col="red")
 
