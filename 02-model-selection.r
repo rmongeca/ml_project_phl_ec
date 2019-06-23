0f5939ea653388d98c1e5d465dec7ad9f84a9a1c
Ricard Monge <rikymonge@gmail.com>
2019-06-23 19:34:38 +0200

    Updated model selection script and image.

--- 02-model-selection.r
+++ 02-model-selection.r
@@ -10,7 +10,7 @@ library(randomForest)
 library(TunePareto)
 
 ## Variables to select dataset to test
-oversampled <- FALSE
+oversampled <- TRUE
 var <- 'cat'
 
 ## Get taining data according to dataset decision
@@ -41,11 +41,12 @@ if(var == 'num') {
 lda.model <- lda (x=data[, -c(target)], grouping=data[,target], CV=TRUE)
 (ct <- table(Truth=data[,target], Pred=lda.model$class))
 ## Really low habitable accuracy, really high false positives
-diag(prop.table(ct, 1))
-print("Habitable accuracy:")
-diag(prop.table(ct, 1))[1]
+print("Habitable class accuracy:")
+prop.table(ct, 1)[1,1]
 print("Habitable false negative rate:")
 prop.table(ct,1)[1,2]
+print("Model general accuracy:")
+sum(diag(prop.table(ct)))
 
 ################### QDA MODEL ###################
 
@@ -64,20 +65,21 @@ if(var == 'num') {
 ## Predition model
 qda.model <- qda (x=data[, -c(target)], grouping=data[,target], CV=TRUE)
 (ct <- table(Truth=data[,target], Pred=qda.model$class))
-## Really low habitable accuracy, really high false positives
-diag(prop.table(ct, 1))
-print("Habitable accuracy:")
-diag(prop.table(ct, 1))[1]
+
+print("Habitable class accuracy:")
+prop.table(ct, 1)[1,1]
 print("Habitable false negative rate:")
 prop.table(ct,1)[1,2]
+print("Model general accuracy:")
+sum(diag(prop.table(ct)))
 
 ################### NAIVE BAYES MODEL ###################
 
 set.seed(42)
 k <- 5
 folds <- generateCVRuns(data$P_HABITABLE, ntimes=1, nfold=k, stratified=TRUE)
-cv.results <- matrix (rep(0,3*k),nrow=k)
-colnames (cv.results) <- c("fold","Hab.accur","Hab.FN")
+cv.results <- matrix (rep(0,4*k),nrow=k)
+colnames (cv.results) <- c("fold","Hab.accur","Hab.FN","Model.accur")
 
 ## CV loop for accuracy measures
 for(i in 1:k) {
@@ -87,18 +89,21 @@ for(i in 1:k) {
   ## Get predictions accuracy measures for fold
   pred <- predict(naive.model, newdata=data[va,])
   ct <- table(Truth=data[va,target], Pred=pred)
-  cv.results[i,"Hab.accur"] <- diag(prop.table(ct, 1))[1]
+  cv.results[i,"Hab.accur"] <- prop.table(ct, 1)[1,1]
   cv.results[i,"Hab.FN"] <- prop.table(ct,1)[1,2]
+  cv.results[i,"Model.accur"] <- sum(diag(prop.table(ct)))
   cv.results[i,"fold"] <- i
   
 }
 rm(i, k, folds, pred, va)
 
 ## Get predictions accuracy measures
-print("Habitable accuracy:")
+print("Habitable class accuracy:")
 mean(cv.results[,"Hab.accur"])
 print("Habitable false negative rate:")
 mean(cv.results[,"Hab.FN"])
+print("Model general accuracy:")
+mean(cv.results[,"Model.accur"])
   
 
 ################### RANDOM FOREST MODEL ###################
@@ -112,10 +117,10 @@ folds <- generateCVRuns(data$P_HABITABLE, ntimes=seq.times, nfold=k, stratified=
 mtry <- seq(2,ncol(data)-1,length.out = seq.times)
 mtry <- as.integer(mtry)
 
-seq.results <- matrix(rep(0,3*seq.times),nrow=seq.times)
-colnames (seq.results) <- c("Mtry","Hab.accur","Hab.FN")
-cv.results <- matrix (rep(0,3*k),nrow=k)
-colnames (cv.results) <- c("fold","Hab.accur","Hab.FN")
+seq.results <- matrix(rep(0,4*seq.times),nrow=seq.times)
+colnames (seq.results) <- c("Mtry","Hab.accur","Hab.FN","Model.accur")
+cv.results <- matrix (rep(0,4*k),nrow=k)
+colnames (cv.results) <- c("fold","Hab.accur","Hab.FN","Model.accur")
 
 ## Paramter tuning loop 
 for(j in 1:seq.times) {
@@ -130,18 +135,20 @@ for(j in 1:seq.times) {
     cv.results[i,"fold"] <- i
     cv.results[i,"Hab.accur"] <- diag(prop.table(ct, 1))[1]
     cv.results[i,"Hab.FN"] <- prop.table(ct,1)[1,2]
+    cv.results[i,"Model.accur"] <- sum(diag(prop.table(ct)))
     
   }
   ## Get predictions accuracy measures
-  seq.results[j,"Hab.accur"] <- diag(prop.table(ct, 1))[1]
-  seq.results[j,"Hab.FN"] <- prop.table(ct,1)[1,2]
+  seq.results[j,"Hab.accur"] <- mean(cv.results[,"Hab.accur"])
+  seq.results[j,"Hab.FN"] <- mean(cv.results[,"Hab.FN"])
+  seq.results[j,"Model.accur"] <- mean(cv.results[,"Model.accur"])
   
 }
 rm(j, i, pred, va, ct, mtry)
 
 ## Get tuned parameter
 plot(seq.results, type='b', col='red')
-seq.results <- seq.results[order(-seq.results[,2]),]
+seq.results <- seq.results[order(-seq.results[,2], seq.results[,1]),]
 mtry <- seq.results[1,1]
 
 
@@ -153,10 +160,10 @@ folds <- generateCVRuns(data$P_HABITABLE, ntimes=seq.times, nfold=k, stratified=
 ntree <- seq(100,500,length.out = seq.times)
 ntree <- as.integer(ntree)
 
-seq.results <- matrix(rep(0,3*seq.times),nrow=seq.times)
-colnames (seq.results) <- c("ntree","Hab.accur","Hab.FN")
-cv.results <- matrix (rep(0,3*k),nrow=k)
-colnames (cv.results) <- c("fold","Hab.accur","Hab.FN")
+seq.results <- matrix(rep(0,4*seq.times),nrow=seq.times)
+colnames (seq.results) <- c("ntree","Hab.accur","Hab.FN","Model.accur")
+cv.results <- matrix (rep(0,4*k),nrow=k)
+colnames (cv.results) <- c("fold","Hab.accur","Hab.FN","Model.accur")
 
 ## Paramter tuning loop 
 for(j in 1:seq.times) {
@@ -171,25 +178,28 @@ for(j in 1:seq.times) {
     cv.results[i,"fold"] <- i
     cv.results[i,"Hab.accur"] <- diag(prop.table(ct, 1))[1]
     cv.results[i,"Hab.FN"] <- prop.table(ct,1)[1,2]
+    cv.results[i,"Model.accur"] <- sum(diag(prop.table(ct)))
+    
   }
   ## Get predictions accuracy measures
-  seq.results[j,"Hab.accur"] <- diag(prop.table(ct, 1))[1]
-  seq.results[j,"Hab.FN"] <- prop.table(ct,1)[1,2]
+  seq.results[j,"Hab.accur"] <- mean(cv.results[,"Hab.accur"])
+  seq.results[j,"Hab.FN"] <- mean(cv.results[,"Hab.FN"])
+  seq.results[j,"Model.accur"] <- mean(cv.results[,"Model.accur"])
   
 }
 rm(j, i, k, cv.results, folds, pred, va, ct, ntree)
 
 ## Get tuned parameter
 plot(seq.results, type='b', col='red')
-seq.results <- seq.results[order(-seq.results[,2]),]
+seq.results <- seq.results[order(-seq.results[,2], seq.results[,1]),]
 ntree <- seq.results[1,1]
 rm(seq.results, seq.times)
 
 ## Train tuned random forest and check custom accuracy measures
 k <- 5
 folds <- generateCVRuns(data$P_HABITABLE, ntimes=1, nfold=k, stratified=TRUE)
-cv.results <- matrix (rep(0,3*k),nrow=k)
-colnames (cv.results) <- c("fold","Hab.accur","Hab.FN")
+cv.results <- matrix (rep(0,4*k),nrow=k)
+colnames (cv.results) <- c("fold","Hab.accur","Hab.FN","Model.accur")
 
 ## CV for model accuracy
 for(i in 1:k) {
@@ -202,15 +212,18 @@ for(i in 1:k) {
   cv.results[i,"fold"] <- i
   cv.results[i,"Hab.accur"] <- diag(prop.table(ct, 1))[1]
   cv.results[i,"Hab.FN"] <- prop.table(ct,1)[1,2]
+  cv.results[i,"Model.accur"] <- sum(diag(prop.table(ct)))
   
 }
 rm(i, k, folds, pred, va, ct)
 
 ## Get predictions accuracy measures
-print("Habitable accuracy:")
+print("Habitable class accuracy:")
 mean(cv.results[,"Hab.accur"])
 print("Habitable false negative rate:")
 mean(cv.results[,"Hab.FN"])
+print("Model general accuracy:")
+mean(cv.results[,"Model.accur"])
 
 
 
@@ -223,12 +236,10 @@ if(var == 'num') {
   folds <- generateCVRuns(data$P_HABITABLE, ntimes=neighbors, nfold=kfolds, stratified=TRUE)
   
   ## Create auxiliary to store values
-  cv.results <- matrix (rep(0,3*kfolds),nrow=kfolds)
-  colnames (cv.results) <- c("fold","Hab.accur","Hab.FN")
-  aux <- matrix(rep(0, 3*neighbors), nrow=neighbors)
-  colnames(aux)<-c("# neighbors","mean Hab.accur", "mean Hab.FN")
-  aux[,"mean Hab.accur"]<-0
-  aux[,"mean Hab.FN"]<-0
+  cv.results <- matrix (rep(0,4*kfolds),nrow=kfolds)
+  colnames (cv.results) <- c("fold","Hab.accur","Hab.FN","Model.accur")
+  aux <- matrix(rep(0, 4*neighbors), nrow=neighbors)
+  colnames(aux)<-c("# neighbors","mean Hab.accur", "mean Hab.FN","Model.accur")
   
   ## CV tuning loops
   for(n in 1:neighbors){
@@ -237,18 +248,24 @@ if(var == 'num') {
       knn.model.train <- knn(data[-va,-c(target)], data[va,-c(target)], data[-va,target], k = n)
       ## Get predictions accuracy measures for fold
       ct <- table(Truth=data[va,target], Pred=knn.model.train)
+      cv.results[i,"fold"] <- i
       cv.results[i,"Hab.accur"] <- diag(prop.table(ct, 1))[1]
       cv.results[i,"Hab.FN"] <- prop.table(ct,1)[1,2]
-      cv.results[i,"fold"] <- i
+      cv.results[i,"Model.accur"] <- sum(diag(prop.table(ct)))
       
     }
     ## Get predictions accuracy measures
     aux[n, "# neighbors"] <- n
     aux[n,"mean Hab.accur"] <- mean(cv.results[,2])
     aux[n, "mean Hab.FN"] <- mean(cv.results[,3])
+    aux[n, "Model.accur"] <- mean(cv.results[,4])
     
   }
   rm(i, n, kfolds, va)
+  
+  ## Plot results
+  plot(aux, type='b', col='red')
+  
   ## Print resulting values
   print("Best Habitable accuracy:")
   max.acc <- max(aux[,"mean Hab.accur"])
@@ -257,10 +274,11 @@ if(var == 'num') {
   min.fn <- min(aux[,"mean Hab.FN"])
   print(min.fn)
   
-  # plot results
-  plot(aux, type='b', col='red')
-  
-  (bestk <- aux[which(aux[,2] == max.acc & aux[,3] == min.fn),1])
+  bestk <- aux[which(aux[,2] == max.acc & aux[,3] == min.fn),]
+  print("Best neighbour Model general accuracy:")
+  print(bestk[4])
+  print("Best neighbour number for knn:")
+  print(bestk[1])
 }
 
 
